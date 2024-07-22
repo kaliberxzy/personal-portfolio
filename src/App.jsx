227a339778dbd77de0bd43bb43889e9fd3c32443
Colin <ctberry21@gmail.com>
2024-07-22 09:57:42 -0500

    create project, added tailwind and font

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex w-full h-screen justify-center items-center">
+      <h1 className="text-6xl font-normal">Hello</h1>
+    </div>
+  );
+}
+
+export default App;

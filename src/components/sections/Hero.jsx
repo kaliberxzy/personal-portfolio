4f3250f612a6f902f91627504ab912047720092a
Colin Berry <ctberry21@gmail.com>
2025-03-02 17:31:03 -0600

    fixed UI bug (resume button on mobile) 2

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -82,7 +82,7 @@ const Hero = () => {
               href={resume}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex justify-center items-center text-[#030303] text-xl tracking-wide font-bold rounded-full bg-gradient-to-r from-orange-500 to-[#FF0000] to-80% py-[7px] px-6"
+              className="flex justify-center items-center text-[#030303] text-xl tracking-wide font-bold rounded-full bg-gradient-to-r from-orange-500 to-[#FF0000] to-80% py-[7px] w-[100px]"
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
             >
@@ -93,7 +93,7 @@ const Hero = () => {
               href={"https://www.linkedin.com/in/colintberry/"}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex justify-center items-center group relative rounded-full bg-gradient-to-r from-orange-500 to-[#FF0000] to-80% p-[2px]"
+              className="flex justify-center items-center group relative rounded-full bg-gradient-to-r from-orange-500 to-[#FF0000] to-80% p-[2px] w-[100px]"
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
             >
